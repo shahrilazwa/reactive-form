f3fa01c2e9d3f474626f4e4e843bde70e5d7caf9
shahrilazwa <shahrilazwa94@gmail.com>
2024-07-31 10:41:56 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'reactive-form';
+  genders = ['male', 'female'];
+  signUpForm: FormGroup;
+
+  ngOnInit():void {
+    this.signUpForm = new FormGroup ({
+      'username':new FormControl(null, Validators.required),
+      'email':new FormControl(null),
+      'gender':new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    console.log("form submitted"),
+    console.log(this.signUpForm)
+  }
 }
+
+
